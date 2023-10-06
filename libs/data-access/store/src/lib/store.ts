12ac221bc430a0ff2c602b7947240efd120a7236
Alec Draymore <Draymore26@gmail.com>
2023-10-06 15:14:50 -0400

    Set up redux store

---
+++ libs/data-access/store/src/lib/store.ts
@@ -0,0 +1,15 @@
+import { configureStore } from '@reduxjs/toolkit'
+
+export const store = configureStore({
+  reducer: {  },
+  // Additional middleware can be passed to this array
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
+  devTools: process.env.NODE_ENV !== 'production',
+  // Optional Redux store enhancers
+  enhancers: [],
+});
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch
