7ccb7aa6f74c77d77b16d39f8bc9869df50b494d
Alec Draymore <draymore26@gmail.com>
2024-01-31 20:30:31 -0500

    Update task table setup script

--- tools/scripts/dynamo-db/create-tasks-table.sh
+++ tools/scripts/dynamo-db/create-tasks-table.sh
@@ -8,3 +8,11 @@ aws dynamodb create-table \
     --provisioned-throughput \
         ReadCapacityUnits=5,WriteCapacityUnits=5 \
     --table-class STANDARD
+
+aws dynamodb update-table \
+    --endpoint-url http://localhost:8000 \
+    --table-name Tasks \
+    --attribute-definitions AttributeName=UserId,AttributeType=S \
+    --global-secondary-index-updates \
+        "[{\"Create\":{\"IndexName\": \"UserId-index\",\"KeySchema\":[{\"AttributeName\":\"UserId\",\"KeyType\":\"HASH\"}], \
+        \"ProvisionedThroughput\": {\"ReadCapacityUnits\": 10, \"WriteCapacityUnits\": 5      },\"Projection\":{\"ProjectionType\":\"ALL\"}}}]"
